2122b7538e71121c3daf1a50bad881806fbd1575
LucaBerl <lucasberlingeri4@gmail.com>
2025-05-02 19:56:28 -0300

    Update menuViajes.cpp

--- menúes/menuViajes.cpp
+++ menúes/menuViajes.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+using namespace std;
+#include "menuViajes.h"
 
+void menuViajes::mostrar(){
+
+    viajesManager vManager;
+    int opcion;
+    
+    do{
+    cout << "========================================================" << endl;
+    cout << "        MENÚU VIAJES" << endl;
+    cout << "========================================================" << endl;
+    cout << "1. CREAR NUEVO VIAJE" << endl;
+    cout << "2. VIAJES ACTIVOS" << endl;
+    cout << "3. HISTORIAL DE VIAJES" << endl;
+    cout << "4. VOLVER AL MENÚ PRINCIPAL" << endl;
+    cout << "===============================" << endl;
+    cout << "Ingrese una opcion: ";
+
+    cin >> opcion;
+
+    switch(opcion) {
+            case 1:
+                vManager.crearViaje();             
+                break;
+            case 2:
+                vManager.viajesActivos();
+                break;
+            case 3:
+                vManager.historiaViajes();
+                break;       
+            case 4:
+                return;
+                break;
+            default:
+                cout << "Opción inválida. Por favor, intente de nuevo." << endl;
+        }
+
+      cout << endl << endl;
+
+    } while(opcion != 4);
+}
