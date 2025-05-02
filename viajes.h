7bcd372c8ac44061ea73090feed485d6b8ce74cd
LucaBerl <lucasberlingeri4@gmail.com>
2025-05-02 15:13:24 -0300

    Update viajes.h
    Agregamos atributos a la clase viajes

--- viajes.h
+++ viajes.h
@@ -1,1 +1,21 @@
+#pragma once
+#include "ciudades.h"
+#include "choferes.h"
+#include <ctime>
 
+class Viajes{
+
+private:
+  Choferes _chofer;
+  Ciudades _ciudadOrigen;
+  Ciudades _ciudadDestino;
+  float distancia;
+  struct tm _fechaSalida
+  struct tm _fechaLlegada 
+  char[30] tipoCarga;
+  bool estado;
+  
+public:
+
+
+};
