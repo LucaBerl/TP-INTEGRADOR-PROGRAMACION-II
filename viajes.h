dd5968d1c18060aedbfbe6f0139e0c2733634d1e
LucaBerl <lucasberlingeri4@gmail.com>
2025-05-02 16:52:54 -0300

    Update viajes.h

--- viajes.h
+++ viajes.h
@@ -17,11 +17,11 @@ private:
   
 public:
 
-  // Constructor
+// Constructor
 
 Viajes();
 
-  // Setters:
+// Setters:
 
 void set_chofer(const Choferes &chofer);
 void set_ciudadOrigen(const Ciudades &ciudad_origen);
@@ -31,5 +31,16 @@ void set_fechaSalida(const  tm &fechaSalida);
 void set_fechaLlegada(const  tm &fechaLlegada);
 bool set_tipoCarga(std::string tipoCarga);
 void set_estado(bool estado);
+
+// Getters:
+
+const Choferes& get_chofer() const;
+const Ciudades& get_ciudadOrigen() const;
+const Ciudades& get_ciudadDestino() const;
+float get_distancia() const;
+const tm& get_fechaSalida() const;
+const tm& get_fechaLlegada() const;
+std::string get_tipoCarga() const;
+bool get_estado() const;
 
 };
