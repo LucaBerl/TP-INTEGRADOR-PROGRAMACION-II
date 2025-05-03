332ae77244e6ff94c3875bd71630d4b245419428
LucaBerl <lucasberlingeri4@gmail.com>
2025-05-03 18:07:58 -0300

    Update viajes.cpp

--- TP-EmpresaDeTransporte/viajes/viajes.cpp
+++ TP-EmpresaDeTransporte/viajes/viajes.cpp
@@ -2,29 +2,43 @@
 #include <string>
 #include<iostream>
 
-/// Setters:
+// Constructor
 
+Viajes();
 
+// Setters:
 
-
-
-
-bool Viajes::set_distancia(float distancia){
+void set_chofer(const Choferes &chofer){
+    _chofer = chofer;
+} 
+void set_ciudadOrigen(const Ciudades &ciudad_origen){
+    _ciudadOrigen = ciudad_origen;
+}
+void set_ciudadDestino(const Ciudades &ciudad_destino){
+    _ciudadDestino = ciudad_destino;
+}
+bool set_distancia(float distancia){ 
       if (distancia > 0){
           _distancia = distancia;
           return true;
       }else{return false;}
 }
-void Viajes::set_fechaSalida(const  tm &fechaSalida){
+void set_fechaSalida(const  tm &fechaSalida){
     _fechaSalida = fechaSalida;
 }
-void Viajes::set_fechaLlegada(const  tm &fechaLlegada){
+void set_fechaLlegada(const  tm &fechaLlegada){
     _fechaLlegada = fechaLlegada;
 }
-
-void Viajes::set_estado(bool estado){
+bool set_tipoCarga(std::string tipoCarga){
+    if (tipoCarga.length() < sizeof(_tipoCarga)) {
+        strcpy(_tipoCarga, tipoCarga.c_str());
+        return true;
+    } else { return false;}              
+}
+void set_estado(bool estado){
   _estado = estado;
 }
+
 
 ///Getters:
 
