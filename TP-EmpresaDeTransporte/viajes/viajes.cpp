6e2c704bc6f381fd7966bf61338bbd05dcd4d1b6
LucaBerl <lucasberlingeri4@gmail.com>
2025-05-03 18:11:57 -0300

    Update viajes.cpp

--- TP-EmpresaDeTransporte/viajes/viajes.cpp
+++ TP-EmpresaDeTransporte/viajes/viajes.cpp
@@ -8,34 +8,34 @@ Viajes();
 
 // Setters:
 
-void set_chofer(const Choferes &chofer){
+void Viajes::set_chofer(const Choferes &chofer){
     _chofer = chofer;
 } 
-void set_ciudadOrigen(const Ciudades &ciudad_origen){
+void Viajes::set_ciudadOrigen(const Ciudades &ciudad_origen){
     _ciudadOrigen = ciudad_origen;
 }
-void set_ciudadDestino(const Ciudades &ciudad_destino){
+void Viajes::set_ciudadDestino(const Ciudades &ciudad_destino){
     _ciudadDestino = ciudad_destino;
 }
-bool set_distancia(float distancia){ 
+bool Viajes::set_distancia(float distancia){ 
       if (distancia > 0){
           _distancia = distancia;
           return true;
       }else{return false;}
 }
-void set_fechaSalida(const  tm &fechaSalida){
+void Viajes::set_fechaSalida(const  tm &fechaSalida){
     _fechaSalida = fechaSalida;
 }
-void set_fechaLlegada(const  tm &fechaLlegada){
+void Viajes::set_fechaLlegada(const  tm &fechaLlegada){
     _fechaLlegada = fechaLlegada;
 }
-bool set_tipoCarga(std::string tipoCarga){
+bool Viajes::set_tipoCarga(std::string tipoCarga){
     if (tipoCarga.length() < sizeof(_tipoCarga)) {
         strcpy(_tipoCarga, tipoCarga.c_str());
         return true;
     } else { return false;}              
 }
-void set_estado(bool estado){
+void Viajes::set_estado(bool estado){
   _estado = estado;
 }
 
